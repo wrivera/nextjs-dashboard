fdd5c127c0f07f88cf320ca188a56300d2e4239b
Wilber Rivera <wilber.rivera@vmlyr.com>
2024-08-26 19:55:18 -0500

    fix

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -101,8 +101,11 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
-  
+export async function GET() {
+    return Response.json({
+        message:
+          'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      });
 //   try {
 //     await client.sql`BEGIN`;
 //     await seedUsers();
@@ -116,4 +119,4 @@
 //     await client.sql`ROLLBACK`;
 //     return Response.json({ error }, { status: 500 });
 //   }
-// }
+}
